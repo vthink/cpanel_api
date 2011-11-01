277a057e0dd2af9d0632e77cb62aae9e1a6e40fe
Virtual Think Team <vthinkteam@gmail.com>
2011-11-01 16:52:32 +0800

    third commit

--- config/cpanel_api.php
+++ config/cpanel_api.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['cpanel']['host']     = 'vthink.web.id';
-$config['cpanel']['port']     = 2082;
+$config['cpanel']['host']     = '';
+$config['cpanel']['port']     = ;
 $config['cpanel']['ssl']      = false;
-$config['cpanel']['username'] = 'mtrthk';
-$config['cpanel']['password'] = 'A1k7H8k11O15h8A1';
+$config['cpanel']['username'] = '';
+$config['cpanel']['password'] = '';
