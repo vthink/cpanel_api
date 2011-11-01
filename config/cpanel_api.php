41a0eefb8ab97a5fe6297a7b240145f879eabc93
Virtual Think Team <vthinkteam@gmail.com>
2011-11-01 20:43:27 +0800

    Update config/cpanel_api.php

--- config/cpanel_api.php
+++ config/cpanel_api.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['cpanel']['host']     = '';
-$config['cpanel']['port']     = ;
+$config['cpanel']['host']     = 'x3demob.cpx3demo.com';
+$config['cpanel']['port']     = 2082;
 $config['cpanel']['ssl']      = false;
-$config['cpanel']['username'] = '';
-$config['cpanel']['password'] = '';
+$config['cpanel']['username'] = 'x3demob';
+$config['cpanel']['password'] = 'x3demob';
