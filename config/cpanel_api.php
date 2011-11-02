215f2fa0d143de515d327b86758271a77dbafaff
Virtual Think Team <vthinkteam@gmail.com>
2011-11-03 05:05:42 +0800

    refresh branch

--- config/cpanel_api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['cpanel']['host']     = 'cpanel.demo.cpanel.net';
-$config['cpanel']['port']     = 2082;
-$config['cpanel']['ssl']      = false;
-$config['cpanel']['username'] = 'x3demob';
-$config['cpanel']['password'] = 'x3demob';
